1a8ed03c0c11da707092991b9d1c27fb8a25cb53
mikahla1 <70593878+mikahla1@users.noreply.github.com>
2022-11-30 13:18:21 -0500

    Update Post.java

--- src/main/java/edu/famu/rekkoapi/models/parse/Post.java
+++ src/main/java/edu/famu/rekkoapi/models/parse/Post.java
@@ -1,5 +1,5 @@
 package edu.famu.rekkoapi.models.parse;
-import org.parse4j.ParseClassName;
+import edu.famu.rekkoapi.models.serializable.SerializablePost;
 import org.parse4j.ParseObject;
 
 public class Post extends ParseObject {
@@ -13,7 +13,12 @@ public class Post extends ParseObject {
     public void setID(String id) {put(ID, id);}
     public String getNAME() {return getString(NAME);}
     public void setNAME(String name) {put(NAME, name);}
-
-    
+    public String getDESC() {return getString(DESC);}
+    public void setDESC(String desc) {put(DESC, desc);}
+    public String getTEST() {return getString(TEST);}
+    public void setTEST(String test) {put(TEST, test);}
+    public SerializablePost getSerializable() {
+        return new SerializablePost(getID(), getNAME(), getDESC(), getTEST());
+    }
 
 }
