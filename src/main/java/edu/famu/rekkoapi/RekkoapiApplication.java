3884a42efde557a14bc202c7e6c25e4c6bf4c06b
mikahla1 <70593878+mikahla1@users.noreply.github.com>
2022-12-01 13:23:36 -0500

    update

--- src/main/java/edu/famu/rekkoapi/RekkoapiApplication.java
+++ src/main/java/edu/famu/rekkoapi/RekkoapiApplication.java
@@ -1,7 +1,11 @@
 package edu.famu.rekkoapi;
 
+import edu.famu.rekkoapi.models.parse.Friends;
+import edu.famu.rekkoapi.models.parse.Post;
+import edu.famu.rekkoapi.models.parse.User;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.parse4j.Parse;
+import org.parse4j.util.ParseRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +15,9 @@ public class RekkoapiApplication {
     public static void main(String[] args) {
 
         Dotenv dotenv = Dotenv.configure().filename("env").load();
+        ParseRegistry.registerSubclass(Friends.class);
+        ParseRegistry.registerSubclass(Post.class);
+        ParseRegistry.registerSubclass(User.class);
         Parse.initialize(dotenv.get("PARSE_APP_ID"), dotenv.get("PARSE_REST_ID"));
         SpringApplication.run(RekkoapiApplication.class, args);
 
